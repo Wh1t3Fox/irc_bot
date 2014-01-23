0190de7d8c069da16f0865123a5fdfb53385050f
cawest1221 <cawest1221@gmail.com>
2014-01-22 23:56:57 -0500

    Working on is_up()

--- irc-bot.py
+++ irc-bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import socket
+import urllib2
 import random
 
 class Bot():
@@ -16,38 +17,58 @@ class Bot():
     }
     
     
-    
     def __init__(self):
         self.SERVER.connect((self.CONFIG['server'], self.CONFIG['port']))
         self.login(self.CONFIG)
         self.main()
     
+    
     def send_data(self, msg):
         self.SERVER.send(msg)
+    
     
     def login(self, config):
         self.send_data('NICK %s\r\n' % config['nick'])
         self.send_data('USER %i 8 * :%s\r\n' % (config['ident'], config['nick']))
         self.send_data('JOIN %s\r\n' % config['channel'])
-        
     
+        
     def pong(self, txt):
         self.send_data('PONG :%s' % txt)
+    
         
     def op_user(self):
         self.send_data('MODE %s +o %s\r\n' % (self.CONFIG['channel'], self.CONFIG['master']))
     
+    
+    def is_up(self, site):
+        response = urllib2.urlopen('http://www.isup.me/'+site)
+        if response.read().find("It's just you."):
+            self.send_data("PRIVMSG %s :%s is UP\r\n" % (self.CONFIG['channel'], site))
+        else:
+            self.send_data("PRIVMSG %s :%s is DOWN\r\n" % (self.CONFIG['channel'], site))
+    
+    
+    def commands(self):
+        pass
+    
+    
     def main(self):
         while True:
             data = self.SERVER.recv(1024)
-            tmp = data.split('\n')
             check = data.split(':')
-            user = data.split(':')[1].split('!')[0]
+            user = check[1].split('!')[0]
             print check
             
-            if data.find('PING') != -1:
-                self.pong(check[1])
-            elif data.find('JOIN') and user == self.CONFIG['master']:
-                self.op_user()
-    
+            try:
+                if check[0].find('PING') != -1:
+                    self.pong(check[1])
+                elif check[1].find('JOIN') != -1 and user == self.CONFIG['master']:
+                    self.op_user()
+                elif check[2].find('!isup') != -1:
+                    self.is_up(check[2][5:])
+            except Exception, e:
+                print e
+            
+            
 bot = Bot()
